bc89ad25bbbd89615ead3a3aa0f364eddc7022ce
imaguiraga <imaguiraga@hotmail.com>
2020-04-20 21:28:42 -0400

    refactor

--- packages/pipeline-preview-kyrcoh/src/pipeline-dsl/index.js
+++ packages/pipeline-preview-kyrcoh/src/pipeline-dsl/index.js
@@ -3,8 +3,5 @@ export * from "./tree/pipeline-terminal.js";
 export * from "./tree/pipeline-parallel.js";
 export * from "./tree/pipeline-sequence.js";
 
-/* visitors */
-export * from "./visitor/pipeline-g6-visitor.js";
-export * from "./visitor/pipeline-uid-visitor.js";
 /* parser */
 export * from "./parser/pipeline-parser.js";
