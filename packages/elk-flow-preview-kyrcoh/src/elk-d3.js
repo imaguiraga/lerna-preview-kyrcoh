c5812d1f180d2840fba6934835da501289ab3064
imaguiraga <imaguiraga@hotmail.com>
2020-06-08 03:58:43 -0400

    elkjs

---
+++ packages/elk-flow-preview-kyrcoh/src/elk-d3.js
@@ -0,0 +1,252 @@
+import * as d3 from "d3";
+//const ELK = require('elkjs');
+import ELK from 'elkjs/lib/elk.bundled.js';
+export const elkmodule = {};
+
+// Copies a variable number of methods from source to target.
+const rebind = function(target, source) {
+  var i = 1, n = arguments.length, method;
+  while (++i < n) target[method = arguments[i]] = d3_rebind(target, source, source[method]);
+  return target;
+};
+
+// Method is assumed to be a standard D3 getter-setter:
+// If passed with no arguments, gets the value.
+// If passed with arguments, sets the value and returns the target.
+function d3_rebind(target, source, method) {
+  return function() {
+    var value = method.apply(source, arguments);
+    return value === source ? target : value;
+  };
+}
+
+(function (elk) {
+
+  elk.d3kgraph = function () {
+    return init("kgraph");
+  };
+  function init(type) {
+    var d3elk = {};
+    var dispatch = d3.dispatch("finish");
+    // containers
+    var nodes = [];
+    var links = [];
+    var graph = {}; // internal (hierarchical graph)
+    var ports = function (n) {
+      // by default the 'ports' field
+      return n.ports || [];
+    };
+    var labels = function (n) {
+      return n.labels || [];
+    };
+    var options = {};
+    // dimensions
+    var width = 0;
+    var height = 0;
+    var defaultNodeSize = [10, 10];
+    var defaultPortSize = [4, 4];
+    var transformGroup;
+    // kgraph properties that shall be copied
+    var kgraphKeys = [
+      'x', 'y',
+      'width', 'height',
+      'sourcePoint', 'targetPoint',
+      'properties'
+    ].reduce(function (p, c) { p[c] = 1; return p; }, {});
+    // a function applied after each layout run
+    var applyLayout = function () { };
+
+    // the layouter instance with web worker
+    const layouter1 = new ELK({
+      workerUrl: './elk-worker.min.js'
+    });
+    const layouter = new ELK();
+    /**
+     * Setting the available area, the
+     * positions of the layouted graph
+     * are currently scaled down.
+     */
+    d3elk.size = function (size) {
+      if (!arguments.length) return [width, height];
+      width = size[0];
+      height = size[1];
+      return d3elk;
+    };
+    /**
+     * Sets the group used to perform 'zoomToFit'.
+     */
+    d3elk.transformGroup = function (g) {
+      if (!arguments.length) return transformGroup;
+      transformGroup = g;
+      return d3elk;
+    };
+    d3elk.options = function (opts) {
+      if (!arguments.length) return options;
+      options = opts;
+      return d3elk;
+    };
+
+    /*
+     * KGraph
+     * Allows to use the JSON KGraph format
+     */
+    if (type === "kgraph") {
+      d3elk.nodes = function () {
+        var queue = [null,graph],
+          nodes = [],
+          parent;
+        // note that svg z-index is document order, literally
+        while ((parent = queue.pop()) !== null) {
+          nodes.push(parent);
+          (parent.children || []).forEach(function (c) {
+            queue.push(c);
+          });
+        }
+        return nodes;
+      };
+      d3elk.links = function (nodes) {
+        return d3.merge(nodes.map(function (n) {
+          return n.edges || [];
+        }));
+      };
+
+      d3elk.kgraph = function (root) {
+        applyLayout = d3_kgraph_applyLayout;
+        const options = {
+          "elk.algorithm": "layered",
+          "nodePlacement.strategy": "BRANDES_KOEPF",
+          "org.eclipse.elk.port.borderOffset":10,
+          "org.eclipse.elk.layered.mergeEdges":true,
+          "spacing": 40,
+          "spacing.nodeNodeBetweenLayers": 40,
+          "spacing.edgeNodeBetweenLayers": 40,
+          "spacing.edgeEdgeBetweenLayers": 40,
+          "layering.strategy": "LONGEST_PATH"
+        };
+        // start the layouter
+        layouter.layout(root, {
+          layoutOptions: options,
+          logging: true,
+          measureExecutionTime: true
+        }).then(function (kgraph) {
+          graph = kgraph;
+          applyLayout(kgraph);
+
+        }).catch(function (e) {
+          console.error(e);
+        });
+
+        return d3elk;
+      };
+
+      /**
+       * Apply layout for the kgraph style.
+       * Converts relative positions to absolute positions.
+       */
+      var d3_kgraph_applyLayout = function (kgraph) {
+        zoomToFit(kgraph);
+        var nodeMap = {};
+        // convert to absolute positions
+        toAbsolutePositions(kgraph, { x: 0, y: 0 }, nodeMap);
+        toAbsolutePositionsEdges(kgraph, nodeMap);
+        // invoke the 'finish' event
+        dispatch.call("finish",{ graph: kgraph },kgraph);
+      };
+      var toAbsolutePositions = function (n, offset, nodeMap) {
+        n.x = (n.x || 0) + offset.x;
+        n.y = (n.y || 0) + offset.y;
+        nodeMap[n.id] = n;
+        // the offset for the children has to include padding
+        var childOffset = { x: n.x, y: n.y };
+        if (n.padding) {
+          childOffset.x += n.padding.left || 0;
+          childOffset.y += n.padding.top || 0;
+        }
+        // children
+        (n.children || []).forEach(function (c) {
+          c.parent = n;
+          toAbsolutePositions(c, childOffset, nodeMap);
+        });
+      };
+      var isDescendant = function (node, child) {
+        if(node !== undefined && child !== undefined){
+          var parent = child.parent;
+          while (parent) {
+            if (parent == node) {
+              return true;
+            }
+            parent = parent.parent;
+          }
+        }
+        return false;
+      };
+      var toAbsolutePositionsEdges = function (n, nodeMap) {
+      }
+      //@TODO
+      var toAbsolutePositionsEdgesbak = function (n, nodeMap) {
+        // edges
+        (n.edges || []).forEach(function (e) {
+          // transform edge coordinates to absolute coordinates. Note that
+          //  node coordinates are already absolute and that
+          //  edge coordinates are relative to the source node's parent node
+          //  (unless the target node is a descendant of the source node)
+          var srcNode = nodeMap[e.source];
+          var tgtNode = nodeMap[e.target];
+          var relative = isDescendant(srcNode, tgtNode) ?
+            srcNode : srcNode.parent;
+          var offset = { x: 0, y: 0 };
+          if (relative) {
+            offset.x = relative.x;
+            offset.y = relative.y;
+          }
+          if (relative.padding) {
+            offset.x += relative.padding.left || 0;
+            offset.y += relative.padding.top || 0;
+          }
+          // ... and apply it to the edge
+          if (e.sourcePoint) {
+            e.sourcePoint.x += offset.x || 0;
+            e.sourcePoint.y += offset.y || 0;
+          }
+          if (e.targetPoint) {
+            e.targetPoint.x += offset.x || 0;
+            e.targetPoint.y += offset.y || 0;
+          }
+          (e.bendPoints || []).forEach(function (bp) {
+            bp.x += offset.x;
+            bp.y += offset.y;
+          });
+        });
+        // children
+        (n.children || []).forEach(function (c) {
+          toAbsolutePositionsEdges(c, nodeMap);
+        });
+      };
+    };
+
+    /**
+     * If a top level transform group is specified,
+     * we set the scale such that the available
+     * space is used to its maximum.
+     */
+    function zoomToFit(kgraph) {
+      // scale everything so that it fits the specified size
+      var scale = width / kgraph.width || 1;
+      var sh = height / kgraph.height || 1;
+      if (sh < scale) {
+        scale = sh;
+      }
+      // if a transformation group was specified we
+      // perform a 'zoomToFit'
+      if (transformGroup) {
+        transformGroup.attr("transform", "scale(" + scale + ")");
+      }
+    }
+    // return the layouter object
+    return rebind(d3elk, dispatch, "on");
+  }
+  if (typeof module === "object" && module.exports) {
+    module.exports = elk;
+  }
+  return elk;
+})(elkmodule);
---
+++ packages/elk-flow-preview-kyrcoh/src/elk-preview.js
@@ -0,0 +1,123 @@
+import * as d3 from "d3";
+import {elkmodule} from './elk-d3.js';
+import './elk-style.css';
+
+function viewport() {
+  let e = window,
+    a = 'inner';
+  if (!('innerWidth' in window)) {
+    a = 'client';
+    e = document.documentElement || document.body;
+  }
+  return {
+    width: e[a + 'Width'],
+    height: e[a + 'Height']
+  };
+}
+//debugger;
+let width = viewport().width;
+let height = viewport().height;
+
+let zoom = d3.zoom().on("zoom", redraw); 
+let idfun = function(d) { return d.id; };    
+
+let svg = d3.select("body")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .call(zoom)
+    .append("g");
+// define an arrow head
+svg.append("svg:defs")
+     .append("svg:marker")
+      .attr("id", "end")
+      .attr("viewBox", "0 -5 10 10")
+      .attr("refX", 10)
+      .attr("refY", 0)
+      .attr("markerWidth", 3)        // marker settings
+      .attr("markerHeight", 5)
+      .attr("orient", "auto")
+      .style("fill", "#999")
+      .style("stroke-opacity", 0.6)  // arrowhead color
+     .append("svg:path")
+      .attr("d", "M0,-5L10,0L0,5");
+
+// group shizzle
+let root = svg.append("g");
+
+const options = {
+  "elk.algorithm": "layered",
+  "nodePlacement.strategy": "BRANDES_KOEPF",
+  "org.eclipse.elk.port.borderOffset":10,
+  "org.eclipse.elk.layered.mergeEdges":true,
+  "spacing": 40,
+  "spacing.nodeNodeBetweenLayers": 40,
+  "spacing.edgeNodeBetweenLayers": 40,
+  "spacing.edgeEdgeBetweenLayers": 40,
+  "layering.strategy": "LONGEST_PATH"
+};
+
+let layouter = elkmodule.d3kgraph()
+      .size([width, height])
+      .transformGroup(root)
+      .options(options);
+      
+var layoutGraph;
+// load the data and render the elements
+d3.json("flow.json").then( function(graph) {  
+
+    layoutGraph = graph;
+  
+    layouter.on("finish", function(d) {
+      var nodes = layouter.nodes();
+      var links = layouter.links(nodes);
+  
+      var linkData = root.selectAll(".link")
+        .data(links, idfun);
+		
+      var linkEnter = linkData.enter()
+        .append("path")
+        .attr("class", "link")
+        .attr("marker-end", "url(#end)").attr("d", function(e) {
+          var path = "";
+          var d = e.sections[0];
+          if (d.startPoint && d.endPoint) {
+            path += "M" + d.startPoint.x + " " + d.startPoint.y + " ";
+              (d.bendPoints || []).forEach(function(bp, i) {
+                path += "L" + bp.x + " " + bp.y + " ";
+              });
+            path += "L" + d.endPoint.x + " " + d.endPoint.y + " ";
+          }
+          return path;
+        });
+  
+      var nodeData = root.selectAll(".node")
+          .data(nodes, idfun);
+      
+      var nodeEnter = nodeData.enter()
+          .append("g")
+          .attr("class", function(d) { 
+            if (d.children) return "node compound"; else return "node leaf"; 
+          })
+      .attr("transform", function(d) { return "translate(" + (d.x || 0) + " " + (d.y || 0) + ")"});
+          
+      var atoms = nodeEnter.append("rect")
+          .attr("width", 10)
+          .attr("height", 10)
+          .attr("x", 0)
+          .attr("y", 0)
+      .attr("width", function(d) { return d.width; })
+        .attr("height", function(d) { return d.height; });
+    
+      nodeEnter.append("title")
+          .text(function(d) { return d.id; });  
+    });
+  
+    // start an initial layout
+    layouter.kgraph(graph);
+  
+});
+
+function redraw() {
+  //svg.attr("transform", `translate(${d3.event.translate}) scale(${d3.event.scale})`);
+}
