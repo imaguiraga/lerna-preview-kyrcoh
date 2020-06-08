18b12a4bd0e547737beec6880cf38786eadf92be
imaguiraga <imaguiraga@hotmail.com>
2020-06-08 04:53:16 -0400

    zoom

--- packages/elk-flow-preview-kyrcoh/src/elk-preview.js
+++ packages/elk-flow-preview-kyrcoh/src/elk-preview.js
@@ -20,15 +20,14 @@ let height = viewport().height;
 
 let idfun = function(d) { return d.id; };    
 
+const zoomFn = d3.zoom().on("zoom", function () {
+  svg.attr("transform", d3.event.transform);
+});
 let svg = d3.select("body")
     .append("svg")
     .attr("width", width)
     .attr("height", height)
-    .call(
-      d3.zoom().on("zoom", function () {
-        svg.attr("transform", d3.event.transform);
-      })
-    )
+    .call(zoomFn)
     .append("g");
 // define an arrow head
 svg.append("svg:defs")
