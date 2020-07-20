35799e15bffe0a521f0cee7a1904f3092210e207
imaguiraga <imaguiraga@hotmail.com>
2020-07-20 02:48:19 -0400

    import only needed modules dnamically

--- packages/@myscope/diagram-dsl-core/src/flow-dsl/parser/flow-parser.js
+++ packages/@myscope/diagram-dsl-core/src/flow-dsl/parser/flow-parser.js
@@ -3,46 +3,67 @@
 //import * as escodegen from 'escodegen';
 import * as esprima from 'esprima';
 
-const DEBUG = false;
+const DEBUG = true;
+const MODULE_IDs = new Set([
+  "repeat",
+  "sequence",
+  "optional",
+  "choice",
+  "zeroOrMore",
+  "terminal",
+  "parallel",
+  "TerminalResource",
+  "CompositeResource"
+]);
+
 export function parseDsl(input,dslModule){
   // Parse text
   // eslint-disable-next-line
-  let factoryFn = new Function("module","return new Map();");
-  try {
-    let tree = esprima.parseScript(input);
-    // Modify AST
-    // Extract Identifiers
-    let ids = tree.body.filter((elt) => {return elt.type === 'VariableDeclaration'})
-      .map((elt) => {
-        let decl = elt.declarations[0];
-        let name = decl.id.name;
-        let value = name;
-        if(decl.init.type === "ArrowFunctionExpression" || decl.init.type === "FunctionExpression"){
-          value = name+"()";
-        }
-        return `result.set('${name}',${value});`;
-      });
-      
-    let text =
-      `const {
-        repeat,
-        sequence,
-        optional,
-        choice,
-        zeroOrMore,
-        terminal,
-        parallel
-      } = module;
+  let factoryFn = new Function("dslModule","return new Map();");
+  let variableIds = [];
+  let moduleIds = [];
 
-      ${input}
-      
-      let result = new Map();
-      ${ids.join("\n")}
-      return result;
-    `;
+  let callbackFn = function (elt){
+    // Extract Identifiers
+    if(elt.type === 'VariableDeclaration'){
+      let decl = elt.declarations[0];
+      let name = decl.id.name;
+      let value = name;
+      if(decl.init.type === "ArrowFunctionExpression" || decl.init.type === "FunctionExpression"){
+        value = name+"()";
+      }
+      variableIds.push(`result.set('${name}',${value});`);
+
+    } else if (elt.type === 'CallExpression'){
+      let name = elt.callee.name;
+      moduleIds.push(name);
+    }
+  };
+
+  try {
+    let tree = esprima.parseScript(input,{},callbackFn);
+
+    // Extract call ids
+    // Keep only ids in the default  
+    // Remove duplicates
+    moduleIds = [...new Set(moduleIds)].filter((elt) => {
+      return MODULE_IDs.has(elt);
+    });
+
+    let text =
+`const {
+${moduleIds.join(",\n")}
+} = dslModule;
+
+${input}
+
+let result = new Map();
+${variableIds.join("\n")}
+return result;
+`;
     if(DEBUG) console.log(text);
     // eslint-disable-next-line
-    factoryFn = new Function("module",text);
+    factoryFn = new Function("dslModule",text);
 
   } catch(e) {
     console.error(e.name + ': ' + e.message);
